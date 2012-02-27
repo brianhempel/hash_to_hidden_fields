cb020d0d9cce412461df01bec33f458fa73b11f0
Brian Hempel <bnhempel@mtu.edu>
2012-02-27 18:16:00 -0500

    Test that element order is preserved in arrays

--- spec/hash_to_hidden_fields_spec.rb
+++ spec/hash_to_hidden_fields_spec.rb
@@ -17,21 +17,27 @@ test_hash = {
   ]
 }
 
-expected_html = <<HTML
-<input id="utf8" name="utf8" type="hidden" value="✓" />
-<input id="_method" name="_method" type="hidden" value="put" />
-<input id="something_nested1" name="something[nested1]" type="hidden" value="3" />
-<input id="something_nested2" name="something[nested2]" type="hidden" value="4" />
+# Ruby 1.8.7 doesn't have an ordered hash, hence this mess
+# want to make sure arrays come back in the correct order
+expected_html_blocks = [
+'<input id="utf8" name="utf8" type="hidden" value="✓" />',
+'<input id="_method" name="_method" type="hidden" value="put" />',
+'<input id="something_nested1" name="something[nested1]" type="hidden" value="3" />',
+'<input id="something_nested2" name="something[nested2]" type="hidden" value="4" />',
+<<-HTML,
 <input id="array_" name="array[]" type="hidden" value="1" />
 <input id="array_" name="array[]" type="hidden" value="2" />
 <input id="array_" name="array[]" type="hidden" value="3" />
 <input id="array_" name="array[]" type="hidden" value="abc" />
-<input id="array_of_hashes__key_1" name="array of hashes[][key 1]" type="hidden" value="val 1" />
+HTML
+'<input id="array_of_hashes__key_1" name="array of hashes[][key 1]" type="hidden" value="val 1" />',
+<<-HTML,
 <input id="array_of_hashes__ooh__nes_____ting__" name="array of hashes[][ooh, nes&lt;&gt;&lt;&gt;&lt;ting!][]" type="hidden" value="x" />
 <input id="array_of_hashes__ooh__nes_____ting__" name="array of hashes[][ooh, nes&lt;&gt;&lt;&gt;&lt;ting!][]" type="hidden" value="z" />
 <input id="array_of_hashes__ooh__nes_____ting__" name="array of hashes[][ooh, nes&lt;&gt;&lt;&gt;&lt;ting!][]" type="hidden" value="y" />
 HTML
-expected_html.chomp!
+]
+expected_html_blocks.map!(&:chomp)
 
 describe ActionView::Base do
   describe "#hash_to_hidden_fields" do
@@ -41,13 +47,13 @@ describe ActionView::Base do
 
     it "produces the includes the correct tags" do
       # Ruby 1.8.7 doesn't have an ordered hash, hence this mess
-      expected_html.lines.each do |tag|
-        @hash_to_hidden_fields.should include(tag.chomp)
+      expected_html_blocks.each do |tags|
+        @hash_to_hidden_fields.should include(tags.chomp)
       end
     end
 
     it "is the right size" do
-      @hash_to_hidden_fields.size.should == expected_html.size
+      @hash_to_hidden_fields.size.should == expected_html_blocks.join("\n").size
     end
   end
 end
